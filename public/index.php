2d7153168d05c3e82236d44a47dfb24fc93242ba
bienk <sb2394@njit.edu>
2019-03-11 18:13:21 -0400

    Feature: Added Main and CSV classes

--- public/index.php
+++ public/index.php
@@ -6,3 +6,33 @@
  * Time: 12:07 AM
  */
 
+main::start("example.csv");
+
+class main  {
+
+    static public function start($filename) {
+
+        $records = csv::getRecords($filename);
+
+        print_r($records);
+    }
+}
+
+class csv {
+
+    static public function getRecords($filename) {
+
+        $file = fopen($filename,"r");
+
+        while(! feof($file))
+        {
+
+            $record = fgetcsv($file);
+
+            $records[] = $record;
+        }
+
+        fclose($file);
+        return $records;
+    }
+}
