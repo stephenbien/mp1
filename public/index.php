7e1c3d6b7722ae73319048fd33e823eaafa2c9e9
bienk <sb2394@njit.edu>
2019-03-11 19:53:09 -0400

    Feature: Added record and recordFactory classes

--- public/index.php
+++ public/index.php
@@ -13,8 +13,11 @@ class main  {
     static public function start($filename) {
 
         $records = csv::getRecords($filename);
+        foreach ($records as $record) {
+            $array = $record->returnArray();
+            print_r($array);
+        }
 
-        print_r($records);
     }
 }
 
@@ -24,15 +27,59 @@ class csv {
 
         $file = fopen($filename,"r");
 
+        $fieldNames = array();
+
+        $count = 0;
+
         while(! feof($file))
         {
 
             $record = fgetcsv($file);
-
-            $records[] = $record;
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
         }
 
         fclose($file);
         return $records;
+
+    }
+}
+
+class record {
+
+    public function __construct(Array $fieldNames = null, $values = null )
+    {
+        $record = array_combine($fieldNames, $values);
+
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+
+    public function returnArray() {
+        $array = (array) $this;
+
+        return $array;
+    }
+
+    public function createProperty($name = 'first', $value = 'keith') {
+
+        $this->{$name} = $value;
+
+    }
+}
+
+class recordFactory {
+
+    public static function create(Array $fieldNames = null, Array $values = null) {
+
+        $record = new record($fieldNames, $values);
+
+        return $record;
+
     }
 }
